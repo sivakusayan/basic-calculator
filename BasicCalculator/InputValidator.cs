fe3bcb1a0caaf75f9ee3c417f879057abf4cefbc
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-14 13:02:27 -0400

    Implemented InputValidator

---
+++ BasicCalculator/InputValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    class InputValidator
+    {
+        /**
+         * Reads an integer from user input. Throws a Data.ConstraintException if
+         * the input cannot be parsed.
+         **/ 
+        public int ReadInt()
+        {
+            int input;
+            bool isNumeric = int.TryParse(Console.ReadLine(), out input);
+
+            if (!isNumeric) throw new System.Data.ConstraintException();
+
+            return input;
+        }
+    }
+}
--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace BasicCalculator
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
