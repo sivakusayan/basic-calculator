d7a945f75b1568c450d30452cbfcfe2caad35bc0
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-07-14 13:48:29 -0400

    Program now asks and validates for two number inputs.

---
+++ BasicCalculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+
+    class Calculator
+    {
+        static void showErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Input. Shutting down application.");
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            InputValidator reader = new InputValidator();
+            int a;
+            int b;
+
+            try
+            {
+                Console.WriteLine("Please enter two integers.");
+                a = reader.ReadInt();
+                b = reader.ReadInt();
+            } catch
+            {
+                showErrorMessage();
+                System.Environment.Exit(1);
+            }
+        }
+    }
+}
